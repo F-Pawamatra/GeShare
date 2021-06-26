d4641db08ef798dc44371ed5af0336250057ade9
F-Pawamatra <72060401+F-Pawamatra@users.noreply.github.com>
2021-06-26 19:57:19 +0700

    menambahkan page choose store

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:ge_share/screens/pasien/pasien_profile_view.dart';
 import 'package:ge_share/screens/donatur/donatur_dashboard_view.dart';
 import 'package:ge_share/screens/donatur/donatur_profile_view.dart';
 import 'package:ge_share/screens/donatur/donate_now_part.dart';
+import 'package:ge_share/screens/donatur/choose_store.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
@@ -14,13 +15,14 @@ void main() {
     title: "Login Register Page",
     initialRoute: "/",
     routes: {
-      "/" : (context) => LoginPage(),
+      "/" : (context) => ChooseStorePage(),
       RegisterPage.routeName : (context) => RegisterPage(),
       PasienDashboardPage.routeName : (context) => PasienDashboardPage(),
       PasienProfilePage.routeName : (context) => PasienProfilePage(),
 
       DonaturDashboardPage.routeName : (context) => DonaturDashboardPage(),
       DonaturProfilePage.routeName : (context) => DonaturProfilePage(),
+      ChooseStorePage.routeName : (context) => ChooseStorePage(),
       // CalonPenerimaDonasi.routeName : (context) => CalonPenerimaDonasi(),
     },
   ));
